6f75db02b00e1af739046d89499938e3a9c43c73
marianom24 <marianojosmoreno@gmail.com>
2024-09-25 16:37:52 -0300

    Fixing wrong name classes in Hamburger Menu component

--- src/HamburgerMenu.jsx
+++ src/HamburgerMenu.jsx
@@ -18,28 +18,28 @@ export function HamburgerMenu(){
                     <span className="text-2xl font-semibold text-white">Mariano</span>
                 </a>
                 <ToggleLanguage/>
-                <button type="button" onClick={handleShowMenu} class="inline-flex items-center justify-center p-2 w-10 h-10 text-sm rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 text-gray-400" aria-expanded="false">
+                <button type="button" onClick={handleShowMenu} className="inline-flex items-center justify-center p-2 w-10 h-10 text-sm rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 text-gray-400" aria-expanded="false">
                     <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
                     </svg>
                 </button>
                 {
                     showMenu && (
-                        <div claclassNamess="w-full h-auto z-10 absolute items-center top-16 left-0 pb-6 text-center  bg-slate-800" id="navbar-hamburger">
+                        <div className="w-full h-auto z-10 absolute items-center top-16 left-0 pb-6 text-center bg-slate-800" id="navbar-hamburger">
 
                             <ul className="flex flex-col font-medium pt-6 rounded-lg">
                                 
                                 <li>
-                                <a href="#about-section" onClick={handleShowMenu} class="block py-2 px-3 text-2xl rounded hover:bg-gray-100 text-gray-400 md:hover:text-white hover:text-white">Mariano</a>
+                                <a href="#about-section" onClick={handleShowMenu} className="block py-2 px-3 text-2xl rounded hover:bg-gray-100 text-gray-300 hover:text-black">Mariano</a>
                                 </li>
                                 <div className="bg-white h-[1px] bg-opacity-15 "></div>
                                 <li>
-                                <a href="#project-section" onClick={handleShowMenu} class="block py-2 px-3  text-2xl  rounded hover:bg-gray-100 text-gray-400  hover:text-white">{t("Projects")}</a>
+                                <a href="#project-section" onClick={handleShowMenu} className="block py-2 px-3  text-2xl  rounded hover:bg-gray-100 text-gray-300 hover:text-black">{t("Projects")}</a>
                                 </li>
                                 <div className="bg-white h-[1px] bg-opacity-15 "></div>
 
                                 <li>
-                                <a href="#about-section" onClick={handleShowMenu} class="block py-2 px-3 text-2xl  rounded  hover:bg-gray-100 text-gray-400 md:hover:text-white hover:text-whit">{t("About me")}</a>
+                                <a href="#about-section" onClick={handleShowMenu} className="block py-2 px-3 text-2xl  rounded  hover:bg-gray-100 text-gray-300 hover:text-black">{t("About me")}</a>
                                 </li>
                             </ul>
                         </div>
